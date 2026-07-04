a3bbccf5c3a93b1c1e54f09443de0e4372a81243
Anshikasaxenaa <anshikasaxena314@gmail.com>
2026-07-04 19:36:35 +0530

    Debugged the errors

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -18,18 +18,17 @@ async function dbConnect(): Promise<void> {
     }
 
     try{
+        console.log("MONGODB_URI =", process.env.MONGODB_URI);
         const db = await mongoose.connect(process.env.MONGODB_URI || '', {})
 
         connection.isConnected= db.connections[0].readyState
 
         console.log("Db connected successfully");
         
-    }catch(error){
-
-        console.log("Database connection failed", error);
-        
-        process.exit(1)
-    }
+    }catch (error) {
+    console.error("Database connection failed:", error);
+    throw error;
+}
 }
 
 
